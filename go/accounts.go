ac3f760e395fe954831c222b7ca6d02d91102725
SIDANWhatever <wongkahinhinson@gmail.com>
2025-04-16 17:52:17 +0800

    feat: init golang sdk example

---
+++ go/accounts.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	deltadefi "github.com/deltadefi-protocol/go-sdk"
+	"github.com/lpernett/godotenv"
+)
+
+func main() {
+	accounts()
+}
+
+func accounts() {
+	godotenv.Load()
+	config := deltadefi.ApiConfig{
+		Network:           "staging",
+		ApiKey:            os.Getenv("DELTADEFI_API_KEY"),
+		OperationPasscode: os.Getenv("ENCRYPTION_PASSCODE"),
+	}
+	client := deltadefi.NewDeltaDeFi(config)
+
+	accountBalanceRes, _ := client.Account.GetAccountBalance()
+	fmt.Println("\nAccount Balance:")
+	for _, record := range *accountBalanceRes {
+		fmt.Println(record)
+	}
+
+	fmt.Println("\nDeposit Records:")
+	depositRecordRes, _ := client.Account.GetDepositRecords()
+	for _, record := range *depositRecordRes {
+		fmt.Println(record)
+	}
+
+	fmt.Println("\nWithdrawal Records:")
+	withdrawalRecordRes, _ := client.Account.GetWithdrawalRecords()
+	for _, record := range *withdrawalRecordRes {
+		fmt.Println(record)
+	}
+
+	fmt.Println("\nOrder Records:")
+	orderRecordsRes, _ := client.Account.GetOrderRecords()
+	fmt.Printf("Order Records: %v\n", orderRecordsRes)
+	for _, record := range orderRecordsRes.Orders {
+		fmt.Println(record)
+	}
+}
